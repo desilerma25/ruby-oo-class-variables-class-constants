a93eab5b71f295baac6e839300bb3227f3d53257
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-03 22:27:59 -0600

    commit tree 324f550de83b06bdb22d93e1cb8ebe18eeae254e

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre # we want genre to be explicilty written, so we give it a reader
+
+  GENRES = [] # creates a class constant
 
   def initialize(title)
     @title = title
@@ -8,6 +10,11 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre) # here we explicitly create the writer method
+    @genre = genre 
+    GENRES << genre # shovel genres into our class constant array
   end
 
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,22 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+     # upon initialize., shovel the brand into BRANDS
+     # we only want unique, so unless BRANDS (already) include? brand
+    BRANDS << brand unless BRANDS.include?(brand) 
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def brand=(brand)
+    @brand = brand
   end
 
 end
